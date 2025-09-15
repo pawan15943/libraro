ba67a2db6991323d6404b421afc2ddd9e1e68352
heena.kaushar@allenoverseas.com-at-026137225090 <heena.kaushar@allenoverseas.com>
2025-09-15 22:29:29 +0530

    update validation

--- app/Http/Controllers/QrEntryController.php
+++ app/Http/Controllers/QrEntryController.php
@@ -595,7 +595,7 @@ class QrEntryController extends Controller
         $branch = Branch::where('uuid', $uuid)->firstOrFail();
 
         $customer = Learner::where('branch_id', $branch->id)
-            ->where('mobile', $request->mobile)
+            ->where('mobile', encryptData($request->input('mobile')))
             ->first();
 
         if (!$customer) {
