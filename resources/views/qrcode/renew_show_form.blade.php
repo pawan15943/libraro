2138894ef86d497a05debe11acbbbeb8212a43bd
heena.kaushar@allenoverseas.com-at-026137225090 <heena.kaushar@allenoverseas.com>
2025-09-15 23:45:18 +0530

    debug query15

--- resources/views/qrcode/renew_show_form.blade.php
+++ resources/views/qrcode/renew_show_form.blade.php
@@ -1,4 +1,88 @@
-<h4>Renew Plan for {{ $customer->name }}</h4>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library managment</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="http://localhost/genrate/public/css/home-style.css">
+
+    <style>
+        .process-step-1,
+        .process-step-2 {
+            height: 100vh;
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+            gap: 1rem;
+            justify-content: space-between;
+        }
+
+        .sacnd-data {
+            background: linear-gradient(2deg, #d6faff, transparent);
+        }
+
+        .action-content {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 1rem;
+        }
+
+        .action-content span.text-message {
+            color: #a1a1a1;
+            font-size: .9rem;
+        }
+
+        .sacnd-data span.footer {
+            font-size: .8rem;
+        }
+
+        input.btn.btn-primary {
+            background: #18225f;
+            border-color: #18225f;
+        }
+
+        ul.action-list {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 1.5rem;
+            list-style: none;
+            padding: 0;
+            justify-content: space-between;
+        }
+
+        ul.action-list li {
+            width: calc(100% / 2 - .75rem);
+
+        }
+
+        ul.action-list li a {
+            text-decoration: none;
+            display: block;
+            text-align: center;
+            padding: 2rem 2rem;
+            background: #fff;
+            box-shadow: 1px 0 5px #00000021;
+            border-radius: 1rem;
+            font-weight: 700;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="sacnd-data">
+        <div class="container">
+            <h4>Renew Plan for {{ $customer->name }}</h4>
 
 <div class="card p-3">
     <div class="row">
@@ -45,11 +129,9 @@
     </h5>
 </div>
 @endif
-
-<div class="text-center mt-4">
-    <form action="{{ route('booking.store', $branch->uuid) }}" method="POST">
-    
-        @csrf
+            <!-- resources/views/booking/form.blade.php -->
+            <form action="{{ route('booking.store', $branch->uuid) }}" method="POST">
+               @csrf
 
         {{-- Required fields for validation --}}
         <input type="hidden" name="renewal" value="1">
@@ -74,5 +156,21 @@
         <input type="hidden" name="learner_transaction_id" value="{{ $transaction->id }}">
 
         <button type="submit" class="btn btn-success">Confirm Renewal</button>
-    </form>
-</div>
+            </form>
+
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    <!-- DataTables JS -->
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js"></script>
+   
+
+
+</body>
+
+</html>
