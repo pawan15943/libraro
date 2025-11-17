b3beddeeb6688e6106ad6ca9f13c1850df8648de
heena.kaushar@allenoverseas.com-at-026137225090 <heena.kaushar@allenoverseas.com>
2025-11-17 23:00:18 +0530

    update floor function

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -1023,27 +1023,29 @@ $alertClass = $completion < 50 ? 'alert-danger' : 'alert-warning' ;
 
                                     <h6>Seat No. : {{$value['seat_no'] ? getSeatDisplayShortFloor($value['seat_no']) : 'GEN'}}</h6>
                                     <small class="capitalise">{{$value->name ?? ''}} </small>
-                                    @if($value->planType->name=='First Half')
-                                    <small>(FH)</small>
-                                    @elseif($value->planType->name=='Second Half')
-                                    <small>(SH) </small>
-                                    @elseif($value->planType->name=='Hourly Slot 1')
-                                    <small>(H1) </small>
-                                    @elseif($value->planType->name=='Hourly Slot 2')
-                                    <small>(H2) </small>
-                                    @elseif($value->planType->name=='Hourly Slot 3')
-                                    <small>(H3) </small>
-                                    @elseif($value->planType->name=='Hourly Slot 4')
-                                    <small>(H4) </small>
-                                    @elseif($value->planType->name=='Full Day')
-                                    <small>(FD) </small>
-                                    @elseif($value->planType->name=='Full Night')
-                                    <small>(FN) </small>
-                                    @elseif($value->planType->name=='All Day')
+                                   
+                                    @if(optional($value->planType)->name == 'First Half')
+                                        <small>(FH)</small>
+                                    @elseif(optional($value->planType)->name == 'Second Half')
+                                        <small>(SH)</small>
+                                    @elseif(optional($value->planType)->name == 'Hourly Slot 1')
+                                        <small>(H1)</small>
+                                    @elseif(optional($value->planType)->name == 'Hourly Slot 2')
+                                        <small>(H2)</small>
+                                    @elseif(optional($value->planType)->name == 'Hourly Slot 3')
+                                        <small>(H3)</small>
+                                    @elseif(optional($value->planType)->name == 'Hourly Slot 4')
+                                        <small>(H4)</small>
+                                    @elseif(optional($value->planType)->name == 'Full Day')
+                                        <small>(FD)</small>
+                                    @elseif(optional($value->planType)->name == 'Full Night')
+                                        <small>(FN)</small>
+                                     @elseif(optional($value->planType)->name=='All Day')
                                     <small>(AD) </small>
                                     @else
-                                    <small>{{ $value->planType->name }}</small>
+                                    <small>{{ optional($value->planType)->name }}</small>
                                     @endif
+
                                     
                                 </div>
                                 <div class="seat-status">
