a82033f326585e612b78b130b8bfff042530ed28
heena.kaushar@allenoverseas.com-at-026137225090 <heena.kaushar@allenoverseas.com>
2025-12-05 09:45:13 +0530

    update branch id according inquery plan type

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -322,9 +322,9 @@ class SiteController extends Controller
     public function libraryDetail($slug)
     {
         $features = DB::table('features')->whereNull('deleted_at')->get();
-        $library = Branch::where('slug', $slug)->with('state', 'city', 'library.subscription', 'library')->first();
+        $branch = Branch::where('slug', $slug)->with('state', 'city', 'library.subscription', 'library')->first();
 
-        if (empty($library)) {
+        if (empty($branch)) {
             return view('errors.404');
         } else {
            
@@ -339,17 +339,17 @@ class SiteController extends Controller
                     'plan_prices.price',
                     'plans.plan_id'
                 )
-                ->where('plan_prices.branch_id', $library->id) 
+                ->where('plan_prices.branch_id', $branch->id) 
                 ->where('plans.plan_id', 1)
                 ->get();
                
 
-            $total_seat = Hour::withoutGlobalScopes()->where('branch_id', $library->id)->value('seats') ?? 0;
+            $total_seat = Hour::withoutGlobalScopes()->where('branch_id', $branch->id)->value('seats') ?? 0;
 
-            $operating = PlanType::withoutGlobalScopes()->where('library_id', $library->library_id)->where('day_type_id', 1)->select('start_time', 'end_time')->first();
+            $operating = PlanType::withoutGlobalScopes()->where('branch_id', $branch->id)->where('day_type_id', 1)->select('start_time', 'end_time')->first();
 
-            $learnerFeedback = LearnerFeedback::where('library_id', $library->library_id)->with(['learner'])->get();
-            $libraryplantype = PlanType::withoutGlobalScopes()->where('library_id', $library->library_id)->pluck('name', 'id');
+            $learnerFeedback = LearnerFeedback::where('library_id', $branch->library_id)->with(['learner'])->get();
+            $libraryplantype = PlanType::withoutGlobalScopes()->where('branch_id', $branch->id)->pluck('name', 'id');
         }
       
 
