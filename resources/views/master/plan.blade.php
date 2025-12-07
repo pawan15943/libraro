6581b6f9b4e50245cc50cdb86923075130987f37
Pawan Rathore <samvad.pawan@gmail.com>
2025-12-07 19:33:35 +0530

    Changes of UI

--- resources/views/master/plan.blade.php
+++ resources/views/master/plan.blade.php
@@ -35,6 +35,45 @@
         <input type="hidden" name="library_id" value="{{getLibraryId()}}">
         <input type="hidden" name="databasemodel" value="Plan">
         <input type="hidden" name="redirect" value="{{ route('plan.index') }}">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="how-use border p-3 mt-4 bg-light rounded mb-4">
+                    <!-- Collapse Header -->
+                    <h4 class="d-flex justify-content-between align-items-center" 
+                        data-bs-toggle="collapse" 
+                        data-bs-target="#howItWorksCollapse" 
+                        style="cursor:pointer;">How the Libraro Intelligent System Works
+                        <span class="toggle-icon">+</span>
+                    </h4>
+
+                    <!-- Collapsible Content -->
+                    <div id="howItWorksCollapse" class="collapse">
+                        <p class="m-0 pt-3">Dear library owner, </p>
+                        <ul type="1" class="how-it-work">
+                            <li>We have made <b>Plan Management</b> much easier for you.</li>
+                            <li><b>You only need to set up everything for the 1-Month Plan.</b><br> 
+                                Once the 1-Month Price and details are added, the system will take care of the rest.
+                            </li>
+                            <li>
+                                Now you can add plans like <b>2-Month, 3-Month, and more,</b> and you do not need to enter prices for these plans.
+                                Our intelligent system will automatically calculate their prices based on your 1-month plan.
+                            </li>
+                            <li>
+                                <ul>
+                                    <li><b>Plan Type / Shift :</b> <br>
+                                        You can create multiple custom plan types. <br> 
+                                        The system will use your <b>1-Month price</b> to calculate all other durations.
+                                    </li>
+                                    <li><b>Plan Price :</b> <br> 
+                                        If you want to change the price, you can update the custom plan price in the plan price section.
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="row g-4">
             <div class="col-lg-4">
                 <label for="">Type <span>*</span></label>
@@ -65,8 +104,8 @@
 
             <div class="col-lg-4">
                 <label for="">Monthly Days</label>
-               <select class="form-select @error('monthdays') is-invalid @enderror" name="monthdays" id="monthdays" 
-                        data-selected="{{ old('monthdays', isset($plan) ? $plan->monthdays : '') }}">
+                <select class="form-select @error('monthdays') is-invalid @enderror" name="monthdays" id="monthdays"
+                    data-selected="{{ old('monthdays', isset($plan) ? $plan->monthdays : '') }}">
                     <option value="">Select Month Days Option</option>
                     <option value="28">28 Days</option>
                     <option value="30">30 Days</option>
@@ -81,15 +120,16 @@
             </div>
 
 
-           
+
         </div>
         <div class="row mt-4">
-             <div class="col-lg-3">
+            <div class="col-lg-3">
                 <button type="submit" class="btn btn-primary button" id="savePlanBtn"><i class="fa fa-plus"></i>
                     @if(isset($plan)) Edit Plan @else Add Plan @endif
                 </button>
             </div>
         </div>
+
     </form>
 </div>
 
@@ -103,7 +143,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
 
 <script>
-   $(document).ready(function () {
+    $(document).ready(function() {
 
         function updateMonthDays() {
             let type = $("#type").val();
@@ -146,8 +186,7 @@
             // case 1: if selected value exists → keep it
             if (selectedValue !== null && selectedValue !== undefined && selectedValue !== "") {
                 monthDaysDropdown.val(selectedValue);
-            } 
-            else {
+            } else {
                 // case 2: blank/null → auto-select Calendar Wise ("")
                 monthDaysDropdown.val("");
             }
@@ -158,24 +197,37 @@
 
         updateMonthDays(); // run on page load for edit mode
     });
-
 </script>
 
 <script>
     (function($) {
         $(window).on("load", function() {
             $(".contents").mCustomScrollbar({
-                theme: "dark"
-                , scrollInertia: 300
-                , axis: "y"
-                , autoHideScrollbar: false, // Keeps
+                theme: "dark",
+                scrollInertia: 300,
+                axis: "y",
+                autoHideScrollbar: false, // Keeps
             });
         });
     })(jQuery);
-
 </script>
 
+<!-- Toggle + / - Script -->
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        const header = document.querySelector('[data-bs-target="#howItWorksCollapse"]');
+        const icon = header.querySelector(".toggle-icon");
+        const collapseEl = document.getElementById("howItWorksCollapse");
 
+        collapseEl.addEventListener('show.bs.collapse', function () {
+            icon.textContent = "−"; // Minus
+        });
+
+        collapseEl.addEventListener('hide.bs.collapse', function () {
+            icon.textContent = "+"; // Plus
+        });
+    });
+</script>
 <!-- /.content -->
 @include('master.script')
 @endsection
