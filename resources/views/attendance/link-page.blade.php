6e9e8abae0720039d7676a3a2cb5c3c5409fb1dd
heena.kaushar@allenoverseas.com-at-026137225090 <heena.kaushar@allenoverseas.com>
2025-12-14 23:23:18 +0530

    update code according to hubrid3

--- resources/views/attendance/link-page.blade.php
+++ resources/views/attendance/link-page.blade.php
@@ -16,7 +16,7 @@
                         <li><input type="text" id="learner_mobile" placeholder="Mobile Number"></li>
                     </ul>
 
-                    <button   button id="verifyBtn">Next</button>
+                    <button id="verifyBtn">Next</button>
                         <div id="verifyMsg"></div>
 
                         <button id="startScannerBtn" class="btn btn-primary mt-3" style="display:none;">
