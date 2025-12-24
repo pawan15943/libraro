23f54b9c81628b4885e023048c2d78375def4de9
heena.kaushar@allenoverseas.com-at-026137225090 <heena.kaushar@allenoverseas.com>
2025-12-24 17:41:28 +0530

    update qr message

--- resources/views/attendance/attendance-qr.blade.php
+++ resources/views/attendance/attendance-qr.blade.php
@@ -28,7 +28,7 @@
             <p class="text-center text-muted">Scan this QR to Mark your Attendance</p>
             <img id="qrImg" class="img-fluid mb-2" style="max-width:300px;">
             <p id="qrMsg" class="mt-2">
-                <dotlottie-wc src="https://lottie.host/79d3a6d1-4651-47a2-8204-6780dff68b52/BS5YmTvc3K.lottie" style="width: 300px;height: 300px" autoplay loop></dotlottie-wc>
+                {{-- <dotlottie-wc src="https://lottie.host/79d3a6d1-4651-47a2-8204-6780dff68b52/BS5YmTvc3K.lottie" style="width: 300px;height: 300px" autoplay loop></dotlottie-wc> --}}
             </p>
         </div>
 
@@ -40,9 +40,12 @@
             </div>
             <!-- <div id="reader" style="width:300px;height:300px;margin:auto;"></div>
             <button class="btn btn-primary" id="startScanner"> Close Scanner </button> -->
-            <p id="scanMsg" class="mt-2">
+            <p id="scanMsg" class="mt-2"> </p>
+            <p id="successAnimation" style="display:none; text-align:center;">
                 <dotlottie-wc src="https://lottie.host/79d3a6d1-4651-47a2-8204-6780dff68b52/BS5YmTvc3K.lottie" style="width: 300px;height: 300px" autoplay loop></dotlottie-wc>
+
             </p>
+
         </div>
 
     </div>
@@ -163,9 +166,15 @@
 
                 // ✅ Show message
                 document.getElementById('scanMsg').innerText = res.message;
+                        // Hide animation by default
+                document.getElementById('successAnimation').style.display = 'none';
                 // 🔊 PLAY SOUND BASED ON MESSAGE
                 if (res.status === 'success') {
                     audioSuccess.play();
+                     // 🎉 Show animation AFTER sound starts
+                    setTimeout(() => {
+                        document.getElementById('successAnimation').style.display = 'block';
+                    }, 300); // slight delay for better UX
                 } else if (res.status === 'expired') {
                     audioExpired.play();
                 } else {
