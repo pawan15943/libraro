aee90fc58d45b2a6cfd883f25a6a46be1d2a9c89
heena.kaushar@allenoverseas.com-at-026137225090 <heena.kaushar@allenoverseas.com>
2025-12-24 17:56:41 +0530

    update qr message3

--- resources/views/attendance/attendance-qr.blade.php
+++ resources/views/attendance/attendance-qr.blade.php
@@ -37,9 +37,10 @@
             <p class="text-center text-muted">Show your ID card to scanner to mark your Attendance</p>
             <div id="scanner-wrapper">
                 <div id="reader"></div>
-                <p id="successAnimation" style="display:none; text-align:center;">
-                    <dotlottie-wc src="https://lottie.host/79d3a6d1-4651-47a2-8204-6780dff68b52/BS5YmTvc3K.lottie" style="width: 300px;height: 300px" autoplay loop></dotlottie-wc>
-                </p>
+              
+            </div>
+            <div id="successAnimation" style="display:none; text-align:center;" class="mb-4">
+                <dotlottie-wc src="https://lottie.host/79d3a6d1-4651-47a2-8204-6780dff68b52/BS5YmTvc3K.lottie" style="width: 300px; margin: 0 auto;"  autoplay loop></dotlottie-wc>
             </div>
             <!-- <div id="reader" style="width:300px;height:300px;margin:auto;"></div>
             <button class="btn btn-primary" id="startScanner"> Close Scanner </button> -->
@@ -174,7 +175,7 @@
                      // 🎉 Show animation AFTER sound starts
                     setTimeout(() => {
                         document.getElementById('successAnimation').style.display = 'block';
-                       document.getElementById('reader').style.display = 'none';
+                       document.getElementById('scanner-wrapper').style.display = 'none';
                     }, 300); 
                    
                 } else if (res.status === 'expired') {
