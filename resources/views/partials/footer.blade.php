db7de96c085db57a136daf155e081266107ea5de
Pawan Rathore <samvad.pawan@gmail.com>
2026-01-09 23:03:39 +0530

    Changes

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,39 @@
-<div class="footer">@ {{date('Y')}} All Rights Reserved by Libraro.</div>
+<div class="footer d-none d-md-block">@ {{date('Y')}} All Rights Reserved by Libraro.</div>
+<div class="mobile-app">
+    <ul class="mobile-menu">
+        <li>
+            <a href="">
+                <i class="fa fa-home"></i>
+                <span>Home</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="">
+                <i class="fa fa-clock"></i>
+                <span>Register</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="">
+                <i class="fa fa-users"></i>
+                <span>Members</span>
+            </a>
+        </li>
+
+        <li class="active">
+            <a href="" >
+                <i class="fa fa-money-bill"></i>
+                <span>Expenses</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="">
+                <i class="fa fa-headphones"></i>
+                <span>Support</span>
+            </a>
+        </li>
+    </ul>
+</div>
