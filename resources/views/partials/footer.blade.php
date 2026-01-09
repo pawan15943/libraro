5853e9cad3a8623596ab6319669d9a395d95bfef
Pawan Rathore <samvad.pawan@gmail.com>
2026-01-09 23:32:00 +0530

    Changes

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,38 +1,48 @@
 <div class="footer d-none d-md-block">@ {{date('Y')}} All Rights Reserved by Libraro.</div>
 <div class="mobile-app">
     <ul class="mobile-menu">
-        <li>
-            <a href="">
+        <li class="{{ request()->routeIs('library.home') ? 'active' : '' }}">
+            <a href="{{ route('library.home') }}">
                 <i class="fa fa-home"></i>
                 <span>Home</span>
             </a>
         </li>
 
-        <li>
-            <a href="">
-                <i class="fa fa-clock"></i>
-                <span>Register</span>
+        @can('has-permission', 'Search Learner')
+        <li data-bs-toggle="tooltip"
+            data-bs-placement="left"
+            data-bs-title="Search Seat"
+            class="{{ request()->routeIs('learner.search') ? 'active' : '' }}">
+            <a href="{{ route('learner.search') }}">
+                <i class="fa fa-search"></i>
+                <span>Search</span>
             </a>
         </li>
+        @endcan
 
-        <li>
-            <a href="">
-                <i class="fa fa-users"></i>
+        @can('has-permission', 'Book Seat')
+        <li data-bs-toggle="tooltip"
+            data-bs-placement="left"
+            data-bs-title="Book Seat"
+            class="{{ request()->routeIs('seat.book') ? 'active' : '' }}">
+            <a href="javascript:;" class="noseat_popup">
+                <i class="fa fa-chair"></i>
+                <span>Book Seat</span>
+            </a>
+        </li>
+        @endcan
+
+        <li class="{{ request()->routeIs('learners') ? 'active' : '' }}">
+            <a href="{{ route('learners') }}">
+                <i class="fa fa-user-check"></i>
                 <span>Members</span>
             </a>
         </li>
 
-        <li class="active">
-            <a href="" >
-                <i class="fa fa-money-bill"></i>
-                <span>Expenses</span>
-            </a>
-        </li>
-
-        <li>
-            <a href="">
-                <i class="fa fa-headphones"></i>
-                <span>Support</span>
+        <li class="{{ request()->routeIs('seats') ? 'active' : '' }}">
+            <a href="{{ route('seats') }}">
+                <i class="fa fa-network-wired"></i>
+                <span>Seat Map</span>
             </a>
         </li>
     </ul>
