b0f22587c2cecd7777a80a4219f3e5ff004e55d9
heena.kaushar@allenoverseas.com-at-026137225090 <heena.kaushar@allenoverseas.com>
2026-01-30 09:49:06 +0530

    update online offline paylater count

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -551,12 +551,12 @@ class DashboardController extends Controller
         ->count();
         
         $paidQuery=Learner::leftJoin('learner_detail', 'learner_detail.learner_id', '=', 'learners.id')
-        ->where('learners.library_id',getLibraryId())->where('learner_detail.is_paid',1)
+        ->where('learners.library_id',getLibraryId())
         ->where(function ($subQuery) use ( $month , $year) {
             $subQuery->whereYear('plan_start_date', $year)
             ->whereMonth('plan_start_date', $month);
            
-        });
+        })->whereNull('learners.deleted_at');
         // Clone the base query for each payment mode count
         $online_paid = (clone $paidQuery)->where('learner_detail.payment_mode', 1)->count();
         $offline_paid = (clone $paidQuery)->where('learner_detail.payment_mode', 2)->count();
@@ -1161,7 +1161,7 @@ class DashboardController extends Controller
 
             case 'other_paid':
                 //pay later
-                $result = (clone $query)->where('learner_detail.payment_mode', 3)->get();
+                $result = (clone $thismonth_booking)->where('learner_detail.payment_mode', 3)->get();
                 break;
             case 'expired_in_five':
                
