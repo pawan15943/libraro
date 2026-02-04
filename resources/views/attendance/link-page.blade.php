fa0f78ed164a2715b619e4678f596261b7634fb1
heena.kaushar@allenoverseas.com-at-026137225090 <heena.kaushar@allenoverseas.com>
2026-02-04 23:08:40 +0530

    extension attendance debug log add5

--- resources/views/attendance/link-page.blade.php
+++ resources/views/attendance/link-page.blade.php
@@ -105,7 +105,7 @@
 
         if (value === 'dob') {
             input.attr('type', 'date');          // ✅ browser-controlled
-            input.attr('placeholder', 'DD-MM-YYYY');
+            // input.attr('placeholder', 'DD-MM-YYYY');
         } 
         else if (value === 'email') {
             input.attr('placeholder', 'Enter Email ID');
