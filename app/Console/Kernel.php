9d812ccb08d610898ed9030c6de0f79f6d9b56be
heena.kaushar@allenoverseas.com-at-026137225090 <heena.kaushar@allenoverseas.com>
2026-02-24 08:54:52 +0530

    jobs and cron formate create3

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->job(new DailyStatusUpdateJob())
-             ->dailyAt('08:52')
+             ->dailyAt('08:56')
              ->withoutOverlapping();
         // $schedule->command('notifications:update-status')->daily();
         // $schedule->command('data:update')->dailyAt('00:00');
