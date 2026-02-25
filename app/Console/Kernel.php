230c76a182688d5559445de45b8cd94d715bc7e4
heena.kaushar@allenoverseas.com-at-026137225090 <heena.kaushar@allenoverseas.com>
2026-02-25 07:10:56 +0530

    update code for

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->job(new DailyStatusUpdateJob())
-             ->dailyAt('09:01')
+             ->dailyAt('07:15')
              ->withoutOverlapping();
         // $schedule->command('notifications:update-status')->daily();
         // $schedule->command('data:update')->dailyAt('00:00');
