90e918909ff9dbbf3e9574ace0ab22d9b7dc59f1
heena.kaushar@allenoverseas.com-at-026137225090 <heena.kaushar@allenoverseas.com>
2026-03-12 22:17:50 +0530

    update whatapp message

--- app/Helpers/encryption_helper.php
+++ app/Helpers/encryption_helper.php
@@ -2115,3 +2115,11 @@ if (!function_exists('calculatePlanDays')) {
     }
 }
 
+if (!function_exists('changeFormate')) {
+    function changeFormate($date){
+            return \Carbon\Carbon::parse($date)->format('d-m-Y');
+    }
+}
+
+
+
