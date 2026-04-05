0c133a1682b22ee9b1819537340786d5df9a7547
heena.kaushar@allenoverseas.com-at-026137225090 <heena.kaushar@allenoverseas.com>
2026-04-05 14:03:29 +0530

    update cron time2

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     {
         // current 1 command active
          $schedule->command('library:daily-status')
-        ->dailyAt('14:03')
+        ->dailyAt('14:05')
         ->withoutOverlapping()
         ->runInBackground();
 
