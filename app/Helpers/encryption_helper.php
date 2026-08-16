0a3051a4701ca4e82164cd745ee37c23042e33c7
Heena Kaushar <iamheenakaushar@gmail.com>
2026-08-16 10:17:24 +0530

    essage correct for extension

--- app/Helpers/encryption_helper.php
+++ app/Helpers/encryption_helper.php
@@ -878,8 +878,6 @@ if (!function_exists('getUserStatusWithSpan')) {
             return '<span style="color: purple; ">Plan Starts in ' . $startfrom . ' Days</span>';
         } elseif ($diffInDays < 0 && $diffExtendDay > 0) {
             return '<span class="text-danger fs-10 d-block">Extension: ' . abs($diffExtendDay) . ' days left.</span>';
-        } elseif (($diffInDays < 0 && $diffExtendDay == 0)) {
-            return '<span class="text-danger fs-10 d-block">Extension ends today</span>';
         } elseif ($diffInDays == 0) {
             return '<span class="text-warning fs-10 d-block">Plan Expires today</span>';
         } else {
